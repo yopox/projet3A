5ef7a55812548b7c8169ea7db8b541998b8fc277
tipunchetrhum <thibaultdeboutray@gmail.com>
2019-12-12 13:40:53 +0100

    :sparkles: Added stuff to pingtest

--- pingtest/main.cpp
+++ pingtest/main.cpp
@@ -20,19 +20,19 @@
 #include <stdint.h>
 #include <iostream>
 
-#define	DEFDATALEN	(64-ICMP_MINLEN)
-#define	MAXIPLEN	60
-#define	MAXICMPLEN	76
-#define	MAXPACKET	(65536 - 60 - ICMP_MINLEN)
-#define ITERATIONS 100
+#define    DEFDATALEN    (64-ICMP_MINLEN)
+#define    MAXIPLEN    60
+#define    MAXICMPLEN    76
+#define    MAXPACKET    (65536 - 60 - ICMP_MINLEN)
+#define    ITERATIONS 1000
+#define    CELERITY 299792458
 
 using namespace std;
 
 uint16_t in_cksum(uint16_t *addr, unsigned len);
 
 
-int ping(string target)
-{
+long ping(string target) {
 
     int s, i, cc, packlen, datalen = DEFDATALEN;
     struct hostent *hp;
@@ -46,42 +46,36 @@ int ping(string target)
     fd_set rfds;
     struct timeval tv;
     int retval;
-    struct timeval start, end;
-    int end_t;
     bool cont = true;
 
     to.sin_family = AF_INET;
 
     to.sin_addr.s_addr = inet_addr(target.c_str());
-    if (to.sin_addr.s_addr != (u_int)-1)
+    if (to.sin_addr.s_addr != (u_int) -1)
         hostname = target;
-    else
-    {
+    else {
         hp = gethostbyname(target.c_str());
-        if (!hp)
-        {
-            cerr << "unknown host "<< target << endl;
+        if (!hp) {
+            cerr << "unknown host " << target << endl;
             return -1;
         }
         to.sin_family = hp->h_addrtype;
-        bcopy(hp->h_addr, (caddr_t)&to.sin_addr, hp->h_length);
+        bcopy(hp->h_addr, (caddr_t) &to.sin_addr, hp->h_length);
         strncpy(hnamebuf, hp->h_name, sizeof(hnamebuf) - 1);
         hostname = hnamebuf;
     }
     packlen = datalen + MAXIPLEN + MAXICMPLEN;
-    if ( (packet = (u_char *)malloc((u_int)packlen)) == NULL)
-    {
+    if ((packet = (u_char *) malloc((u_int) packlen)) == NULL) {
         cerr << "malloc error\n";
         return -1;
     }
 
 
-    if ( (s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
-    {
+    if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0) {
         return -1; /* Needs to run as superuser!! */
     }
 
-    icp = (struct icmp *)outpack;
+    icp = (struct icmp *) outpack;
     icp->icmp_type = ICMP_ECHO;
     icp->icmp_code = 0;
     icp->icmp_cksum = 0;
@@ -90,16 +84,16 @@ int ping(string target)
 
 
     cc = datalen + ICMP_MINLEN;
-    icp->icmp_cksum = in_cksum((unsigned short *)icp,cc);
+    icp->icmp_cksum = in_cksum((unsigned short *) icp, cc);
 
-    gettimeofday(&start, NULL);
+    // Start Timer
+    auto begin = chrono::high_resolution_clock::now();
 
-    i = sendto(s, (char *)outpack, cc, 0, (struct sockaddr*)&to, (socklen_t)sizeof(struct sockaddr_in));
-    if (i < 0 || i != cc)
-    {
+    i = sendto(s, (char *) outpack, cc, 0, (struct sockaddr *) &to, (socklen_t) sizeof(struct sockaddr_in));
+    if (i < 0 || i != cc) {
         if (i < 0)
             perror("sendto error");
-        cout << "wrote " << hostname << " " <<  cc << " chars, ret= " << i << endl;
+        cout << "wrote " << hostname << " " << cc << " chars, ret= " << i << endl;
     }
 
     FD_ZERO(&rfds);
@@ -107,65 +101,50 @@ int ping(string target)
     tv.tv_sec = 1;
     tv.tv_usec = 0;
 
-    while(cont)
-    {
-        retval = select(s+1, &rfds, NULL, NULL, &tv);
-        if (retval == -1)
-        {
+    while (cont) {
+        retval = select(s + 1, &rfds, NULL, NULL, &tv);
+        if (retval == -1) {
             perror("select()");
             return -1;
-        }
-        else if (retval)
-        {
+        } else if (retval) {
             fromlen = sizeof(sockaddr_in);
-            if ( (ret = recvfrom(s, (char *)packet, packlen, 0,(struct sockaddr *)&from, (socklen_t*)&fromlen)) < 0)
-            {
+            if ((ret = recvfrom(s, (char *) packet, packlen, 0, (struct sockaddr *) &from, (socklen_t *) &fromlen)) <
+                0) {
                 perror("recvfrom error");
                 return -1;
             }
 
             // Check the IP header
-            ip = (struct ip *)((char*)packet);
-            hlen = sizeof( struct ip );
-            if (ret < (hlen + ICMP_MINLEN))
-            {
-                cerr << "packet too short (" << ret  << " bytes) from " << hostname << endl;;
+            ip = (struct ip *) ((char *) packet);
+            hlen = sizeof(struct ip);
+            if (ret < (hlen + ICMP_MINLEN)) {
+                cerr << "packet too short (" << ret << " bytes) from " << hostname << endl;;
                 return -1;
             }
 
             // Now the ICMP part
-            icp = (struct icmp *)(packet + hlen);
-            if (icp->icmp_type == ICMP_ECHOREPLY)
-            {
-                if (icp->icmp_seq != 12345)
-                {
+            icp = (struct icmp *) (packet + hlen);
+            if (icp->icmp_type == ICMP_ECHOREPLY) {
+                if (icp->icmp_seq != 12345) {
                     cout << "received sequence # " << icp->icmp_seq << endl;
                     continue;
                 }
-                if (icp->icmp_id != getpid())
-                {
+                if (icp->icmp_id != getpid()) {
                     cout << "received id " << icp->icmp_id << endl;
                     continue;
                 }
                 cont = false;
-            }
-            else
-            {
+            } else {
                 cout << "Recv: not an echo reply" << endl;
                 continue;
             }
 
-            gettimeofday(&end, NULL);
-            end_t = 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
+            // Stop timer
+            auto end = chrono::high_resolution_clock::now();
+            auto duration = chrono::duration_cast<chrono::nanoseconds>(end - begin).count();
 
-            if(end_t < 1)
-                end_t = 1;
-
-            cout << "Elapsed time = " << end_t << " usec" << endl;
-            return end_t;
-        }
-        else
-        {
+            return duration;
+        } else {
             cout << "No data within one seconds.\n";
             return 0;
         }
@@ -173,8 +152,7 @@ int ping(string target)
     return 0;
 }
 
-uint16_t in_cksum(uint16_t *addr, unsigned len)
-{
+uint16_t in_cksum(uint16_t *addr, unsigned len) {
     uint16_t answer = 0;
     /*
      * Algorithm is simple, using a 32 bit accumulator (sum), add
@@ -182,13 +160,13 @@ uint16_t in_cksum(uint16_t *addr, unsigned len)
      * carry bits from the t   16 bits into the lower 16 bits.
      */
     uint32_t sum = 0;
-    while (len > 1)  {
+    while (len > 1) {
         sum += *addr++;
         len -= 2;
     }
 
     if (len == 1) {
-        *(unsigned char *)&answer = *(unsigned char *)addr ;
+        *(unsigned char *) &answer = *(unsigned char *) addr;
         sum += answer;
     }
 
@@ -200,24 +178,56 @@ uint16_t in_cksum(uint16_t *addr, unsigned len)
 
 
 int main() {
-    std::cout << "Nombre d'itérations : " << ITERATIONS << "\n" << std::endl;
+    cout << "Nombre d'itérations : " << ITERATIONS << "\n" << endl;
 
-    long* results = new long [ITERATIONS];
+    long *results = new long[ITERATIONS];
     long mean = 0;
+    long max = 0;
+    long min = LLONG_MAX;
     for (int i = 0; i < ITERATIONS; ++i) {
-        auto begin = std::chrono::high_resolution_clock::now();
-        auto time = ping("172.17.3.18");
-        auto end = std::chrono::high_resolution_clock::now();
-        results[i] = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
+        auto timelapse = ping("127.0.0.1");
+        if (timelapse == -1) {
+            return -1;
+        } else {
+            results[i] = timelapse;
+        }
         mean += results[i];
-        std::cout << "Iteration #" << i << " : " << results[i] << "ns\n" << std::endl;
+        if (results[i] > max) {
+            max = results[i];
+        }
+        if (results[i] < min) {
+            min = results[i];
+        }
+        cout << "Iteration #" << i << " : " << results[i] << "ns\n" << endl;
     }
-    mean = mean/10;
-    std::cout << "Moyenne sur " << ITERATIONS << " itérations : " << mean << " ns" <<
-    " = " << ((double) mean)/1000       << " us"    <<
-    " = " << ((double) mean)/1000000    << " ms"    <<
-    " = " << ((double) mean)/1000000000 << " s"     << std::endl;
+    mean = mean / ITERATIONS;
+    double distmean = ((double) (mean)) * CELERITY / 1000000000 / 2;
+    double distmax = ((double) (max)) * CELERITY / 1000000000 / 2;
+    double distmin = ((double) (min)) * CELERITY / 1000000000 / 2;
+    double distmaxmin = ((double) (max - min)) * CELERITY / 1000000000 / 2;
 
+    cout << "Moyenne de temps sur " << ITERATIONS << " itérations : " << mean << " ns" <<
+              " = " << ((double) mean) / 1000 << " us" <<
+              " = " << ((double) mean) / 1000000 << " ms" <<
+              " = " << ((double) mean) / 1000000000 << " s" << endl;
+
+    cout << "Distance moyenne sur " << ITERATIONS << " itérations : "<< distmean / 1000 << " km\n" << endl;
+
+    cout << "Max de temps sur " << ITERATIONS << " itérations : " << max << " ns" <<
+              " = " << ((double) max) / 1000 << " us" <<
+              " = " << ((double) max) / 1000000 << " ms" <<
+              " = " << ((double) max) / 1000000000 << " s" << endl;
+
+    cout << "Distance max sur " << ITERATIONS << " itérations : "<< distmax / 1000 << " km\n" << endl;
+
+    cout << "Min de temps sur " << ITERATIONS << " itérations : " << min << " ns" <<
+              " = " << ((double) min) / 1000 << " us" <<
+              " = " << ((double) min) / 1000000 << " ms" <<
+              " = " << ((double) min) / 1000000000 << " s" << endl;
+
+    cout << "Distance min sur " << ITERATIONS << " itérations : "<< distmin / 1000 << " km\n" << endl;
+
+    cout << "Distance équivalente entre max et min " << distmaxmin / 1000 << " km" << endl;
 
     return 0;
 }
