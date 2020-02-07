9135dd3814d691c111daa4bcd8f7ef34f95c9c4d
tipunchetrhum <thibaultdeboutray@gmail.com>
2020-02-07 11:36:01 +0100

    :construction: WIP : trying to add Quiet to app

--- android/app/src/main/java/fr/centralesupelec/db/MainActivity.kt
+++ android/app/src/main/java/fr/centralesupelec/db/MainActivity.kt
@@ -5,11 +5,53 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.quietmodem.Quiet.*
+import java.io.IOException
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_schedule.*
+import org.quietmodem.Quiet.ModemException
+import org.quietmodem.Quiet.NetworkInterfaceConfig
+import org.quietmodem.Quiet.FrameReceiverConfig
+import org.quietmodem.Quiet.FrameTransmitterConfig
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.util.*
+import org.quietmodem.Quiet.LoopbackNetworkInterface
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import org.quietmodem.Quiet.FrameTransmitter
+import org.quietmodem.Quiet.FrameReceiver
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
+
+
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    //private var intf: NetworkInterface? = null
+    private var receiver: FrameReceiver? = null
+    private var transmitter: FrameTransmitter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +59,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         bottom_navigation.setOnNavigationItemSelectedListener(navigationListener)
         openFragment(TodayFragment.newInstance())
+        println("Creating Network Interface")
+        createNetworkInterface()
     }
 
     private val navigationListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -50,12 +94,174 @@ class MainActivity : AppCompatActivity() {
         transaction.commit()
     }
 
+    fun createNetworkInterface() {
+//        var receiverConfig: FrameReceiverConfig? = null
+//        var transmitterConfig: FrameTransmitterConfig? = null
+//
+//        try {
+//            transmitterConfig = FrameTransmitterConfig(
+//                this,
+//                "audible-7k-channel-0"
+//            )
+//            receiverConfig = FrameReceiverConfig(
+//                this,
+//                "audible-7k-channel-0"
+//            )
+//        } catch (e: IOException) {
+//            println("could not build configs")
+//
+//        }
+//
+//
+//        var conf: NetworkInterfaceConfig? = null
+//        try {
+//            conf = NetworkInterfaceConfig(
+//                receiverConfig,
+//                transmitterConfig,
+//                InetAddress.getByName("192.168.0.3"),
+//                InetAddress.getByName("255.255.255.0"),
+//                InetAddress.getByName("192.168.0.1")
+//            )
+//            println(conf.localAddress)
+//
+//        } catch (e: IOException) {
+//            println("could not build network config")
+//        }
+//
+//
+//        try {
+//            intf = NetworkInterface(conf)
+//        } catch (e: ModemException) {
+//            println("network interface threw exception")
+//        }
+        var receiverConfig: FrameReceiverConfig? = null
+        var transmitterConfig: FrameTransmitterConfig? = null
+
+        try {
+            transmitterConfig = FrameTransmitterConfig(
+                this,
+                "audible-7k-channel-0"
+            )
+            receiverConfig = FrameReceiverConfig(
+                this,
+                "audible-7k-channel-0"
+            )
+        } catch (e: IOException) {
+            println("could not build configs")
+        }
+
+
+        try {
+            receiver = FrameReceiver(receiverConfig)
+            transmitter = FrameTransmitter(transmitterConfig)
+        } catch (e: ModemException) {
+            println("could not set up receiver/transmitter")
+        }
+
+
+    }
+
     fun send(view: View) {
-        received.text = ""
+        val payload = "Hello, World!"
+        try {
+            transmitter!!.send(payload.toByteArray())
+        } catch (e: IOException) {
+            println("our message might be too long or the transmit queue full")
+        }
+
+//        var s: Socket? = null
+//        try {
+//            s = Socket()
+//            s.bind(InetSocketAddress("192.168.0.3", 0))
+//        } catch (e: IOException) {
+//            println("new socket failed")
+//        }
+//
+//        try {
+//            s!!.connect(InetSocketAddress("192.168.0.3", 5678))
+//        } catch (e: IOException) {
+//            println(e.message)
+//            println("connect failed")
+//        }
+//
+//
+//        val send = "Hello, World!".toByteArray()
+//        val os = s?.outputStream
+//
+//        try {
+//            os?.write(send)
+//        } catch (e: IOException) {
+//            println("exception sending on Socket")
+//        }
+//
+//        try {
+//            s?.close()
+//        } catch (e: IOException) {
+//            println("close failed")
+//        }
+
     }
 
     fun receive(view: View) {
-        received.text = "test"
+        receiver!!.setBlocking(5, 0);
+        var buf = ByteArray(1024)
+        var recvLen: Long = 0
+        try {
+            recvLen = receiver!!.receive(buf)
+        } catch (e: IOException) {
+            println("read timed out")
+            buf = "".toByteArray()
+        }
+
+        if (buf.isNotEmpty()) {
+            received.text = buf.toString()
+        } else {
+            received.text = "Error"
+        }
+
+//        val listener = Thread(Runnable {
+//            var s: ServerSocket? = null
+//            try {
+//                s = ServerSocket(5678,1,InetAddress.getByName("192.168.0.3"))
+//                println(s.inetAddress)
+//            } catch (e: IOException) {
+//                println("exception creating ServerSocket")
+//            }
+//            val recv = ByteArray(800)
+//
+//            var peer: Socket? = null
+//            try {
+//                peer = s?.accept()
+//            } catch (e: IOException) {
+//                println("accept failed")
+//            }
+//
+//
+//            val `is` = peer!!.inputStream
+//
+//            var recvLen = 0
+//            try {
+//                recvLen = `is`.read(recv)
+//            } catch (e: IOException) {
+//                println("read failed")
+//            }
+
+//
+//            try {
+//                peer.close()
+//                s?.close()
+//                println("server socket closed !")
+//            } catch (e: IOException) {
+//                println("close failed")
+//            }
+//
+//            println(recv.toString())
+//
+//            if (recvLen == recv.size) {
+//                received.text = recv.toString()
+//            }
+//        })
+//        listener.start()
     }
 
 }
